662e8b9211ccc4feab3ffb5a0111101f05e98f64
jeisontc87 <jeisontc87@gmail.com>
2018-05-25 03:20:06 -0300

    tela de login

--- vendor/hcodebr/php-classes/src/Page.php
+++ vendor/hcodebr/php-classes/src/Page.php
@@ -8,7 +8,10 @@
 
 		private $tpl;
 		private $options = [];
-		private $defaults = ["data"=>[]];
+		private $defaults = [
+			"header"=>true,
+			"footer"=>true,
+			"data"=>[]];
 
 		public function __construct($opts = array(), $tpl_dir = "/views/"){
 
@@ -26,7 +29,7 @@
 	$this->setData($this->options["data"]);
 
 	
-	$this->tpl->draw("header");
+	if($this->options["header"]===true) $this->tpl->draw("header");
 
 
 
@@ -50,7 +53,7 @@
 		
 
 		public function __destruct(){
-			$this->tpl->draw("footer");
+			if($this->options["footer"]===true) $this->tpl->draw("footer");
 
 
 		}
